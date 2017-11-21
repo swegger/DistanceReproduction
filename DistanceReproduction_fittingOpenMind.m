74e1883c848b0fdf42454130bfdf146ecbcf7ee8
Seth Egger <swegger@mit.edu>
2017-11-21 11:59:34 -0500

    Added code to allow user to pass fparam structure to DistanceReproduction_fittingOpenMind

--- DistanceReproduction_fittingOpenMind.m
+++ DistanceReproduction_fittingOpenMind.m
@@ -1,4 +1,4 @@
-function DistanceReproduction_fittingOpenMind(SubjectN)
+function DistanceReproduction_fittingOpenMind(SubjectN,varargin)
 %% DistanceReproduction_fittingOpenMind
 %
 %   Fits the BLSbiased model to a set of subjects and (optional) saves
@@ -6,12 +6,14 @@ function DistanceReproduction_fittingOpenMind(SubjectN)
 %
 %%
 
-%% Subject map
+%% Defaults
+
+%% Gobal Variables
+
+% Subject map
 Subjects = {'JW','MD','SM','SS','SWE','TT','VD'};
 runmap = {[2,4,5:8],[2:10],[3:6],[2:9,11],[2:10],[2:10],[2:5]};
 
-%% Variables
-% Gobal Variables
 trialWin = [100 Inf];
 Distance_N = 1:2;
 MinMax = [2 30];
@@ -23,27 +25,27 @@ viewDistance = 310;
 fixPos = 13;
 
 % Fit parameters
-fparams.fittype = {'BLS_wm_wp_sigp','aveMeas_wm_wp_sigp'};     % Specifies which models to fit to the data
-fparams.modelUsed = 1;                                      % Specifies which model to use in subsequent model-based analysis
-fparams.method = 'quad';                                    % Integration method in model fitting
-fparams.init = 'estb';                                      % Initialization values/method for model fitting
-fparams.dx = dt;                                            % Step size of integration (in ms)
+fparams_default.fittype = {'BLS_wm_wp_sigp','aveMeas_wm_wp_sigp'};     % Specifies which models to fit to the data
+fparams_default.modelUsed = 1;                                      % Specifies which model to use in subsequent model-based analysis
+fparams_default.method = 'quad';                                    % Integration method in model fitting
+fparams_default.init = 'estb';                                      % Initialization values/method for model fitting
+fparams_default.dx = dt;                                            % Step size of integration (in ms)
 fparams.trialtypes = [1 2];                                 % Trial types to fit
 
 % Cross validation control
-fparams.CrossValidation.Type = 'LNOCV';                              % Cross validation method
-fparams.CrossValidation.N = 100;                                       % Left out trials for validation
+fparams_default.CrossValidation.Type = 'LNOCV';                              % Cross validation method
+fparams_default.CrossValidation.N = 100;                                       % Left out trials for validation
 
 % Model evidence control
-fparams.ModelEvidence.method = 'none';
-fparams.ModelEvidence.paramLimits = [0.0001 1;...
+fparams_default.ModelEvidence.method = 'none';
+fparams_default.ModelEvidence.paramLimits = [0.0001 1;...
                              0.0001 1;...
                              -200 200;...
                              0 1;...
                              0 100];
-fparams.ModelEvidence.integrationMethod = 'quad';
-fparams.ModelEvidence.integrationOptions.dx = 0.5;
-fparams.ModelEvidence.OpenMind = 1;
+fparams_default.ModelEvidence.integrationMethod = 'quad';
+fparams_default.ModelEvidence.integrationOptions.dx = 0.5;
+fparams_default.ModelEvidence.OpenMind = 1;
 
 % Bias/Variance bootstrap parameters
 bootparams.nbootstraps = 100;
@@ -57,17 +59,46 @@ DAexpectation.simtrials = 10000;
 
 runs = runmap{SubjectN};
 
+%% Parse inputs
+Parser = inputParser;
+
+addRequired(Parser,'SubjectN')
+addParameter(Parser,'SaveOpts',SaveOpts_default)
+addParameter(Parser,'fparams',fparams_default)
+addParameter(Parser,'TestSave',false)
+
+parse(Parser,SubjectN,varargin{:})
+
+SubjectN = Parser.Results.SubjectN;
+SaveOpts = Parser.Results.SaveOpts;
+fparams = Parser.Results.fparams;
+TestSave = Parser.Results.TestSave;
+
+fparams = asign_fparams(fparams);
+if SaveOpts.SaveFlg && ~isfield(SaveOpts,'SaveFile')
+    SaveOpts.SaveFile = ['/om/user/swegger/Projects/DistanceReproduction/'...
+        Subjects{SubjectN} '/' Subjects{SubjectN} '_' ...
+        fparams.fittype{fparams.modelUsed} '_ObsAct' num2str(fparams.ObsAct)...
+        '_' datestr(now,'yyyymmdd')];
+end
+
+
+%%
 % Load the data
 d = load([Subjects{SubjectN} '_DistanceReproduction']);
 
 
-if SaveFlg
-    SaveFileBase = ['/home/swegger/Projects/DistanceReproduction/' Subjects{SubjectN} '/' Subjects{SubjectN} '_BLSbiasedFitResults'];
-    SaveParam = [SaveFileBase datestr(now,'yyyymmdd')];
+% if SaveFlg
+%     SaveFileBase = ['/home/swegger/Projects/DistanceReproduction/' Subjects{SubjectN} '/' Subjects{SubjectN} '_BLSbiasedFitResults'];
+%     SaveParam = [SaveFileBase datestr(now,'yyyymmdd')];
+% else
+%     SaveParam = 'No';
+% end
+if SaveOpts.SaveFlg
+    SaveParam = SaveOpts.SaveFile;
 else
     SaveParam = 'No';
 end
-
 
 % Fit all the data
 if FitAll
@@ -80,3 +111,45 @@ if FitAll
 end
 
 
+%% Functions
+
+    function fparams = asign_fparams(fparams)
+    if ~isfield(fparams,'fittype')
+        fparams.fittype = {'SubOptMemBias','BLSbiasedLapse'};
+    end
+    if ~isfield(fparams,'modelUsed')
+        fparams.modelUsed = 1;                                      % Specifies which model to use in subsequent model-based analysis
+    end
+    if ~isfield(fparams,'method')
+        fparams.method = 'quad';                                    % Integration method in model fitting
+    end
+    if ~isfield(fparams,'init')
+        fparams.init = 'estb';                                      % Initialization values/method for model fitting
+    end
+    if ~isfield(fparams,'dx')
+        fparams.dx = 5;                                            % Step size of integration (in ms)
+    end
+    if ~isfield(fparams,'trialtypes')
+        fparams.trialtypes = [1 2];                                 % Trial types to fit
+    end
+    if ~isfield(fparams,'ObsAct')
+        fparams.ObsAct = 0;
+    end
+
+    % Cross validation control
+    if ~isfield(fparams,'CrossValidation')
+        fparams.CrossValidation.Type = 'LNOCV';                              % Cross validation method
+        fparams.CrossValidation.N = 100;                                       % Left out trials for validation
+    end
+
+    % Model evidence control
+    if ~isfield(fparams,'ModelEvidence')
+        fparams.ModelEvidence.method = 'none';
+        fparams.ModelEvidence.paramLimits = [0.0001 1;...
+            0.0001 1;...
+            -200 200;...
+            0 1];
+        fparams.ModelEvidence.integrationMethod = 'quad';
+        fparams.ModelEvidence.integrationOptions.dx = 0.1;
+        fparams.ModelEvidence.OpenMind = 1;
+    end
